b484ab7d462237e72c94dc7cfbea29cd74963df7
zihco <chizobajames21@gmail.com>
2024-01-25 15:03:11 +0100

    log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""log parsing"""
+import re
+import sys
+
+
+if __name__ == "__main__":
+    pattern = re.compile(r'(\d+\.\d+\.\d+\.\d+) - \[([^\]]+)\]'
+                         r' "GET /projects/260 HTTP/1.1" (\d+) (\d+)')
+    metric_dict = {
+                   '200': 0,
+                   '301': 0,
+                   '400': 0,
+                   '401': 0,
+                   '403': 0,
+                   '404': 0,
+                   '405': 0,
+                   '500': 0
+                  }
+    count = 0
+    total_size = 0
+    line = sys.stdin.readline()
+    while line != '':
+        count += 1
+        if bool(pattern.match(line)):
+            list_line = line.split()
+            status_code = list_line[-2]
+            file_size = list_line[-1]
+            total_size += int(file_size)
+            metric_dict[status_code] += 1
+            if count % 10 == 0:
+                print(f"File size: {total_size}")
+                for key in sorted(metric_dict.keys()):
+                    if metric_dict[key] > 0:
+                        print(f"{key}: {metric_dict[key]}")
+        line = sys.stdin.readline()
