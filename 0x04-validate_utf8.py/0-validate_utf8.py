6f82f949e3cfffe561cfb12521d76fd65e3b4ffe
zihco <chizobajames21@gmail.com>
2024-01-30 15:51:06 +0100

    utf-8 validate

---
+++ 0x04-validate_utf8.py/0-validate_utf8.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""contains the definitions validUTF8"""
+
+
+def validUTF8(data):
+    """return a boolean to validate data utf8"""
+    if type(data) is list:
+        for element in data:
+            if type(element) is int and element < 256:
+                continue
+            else:
+                return False
+        return True
+    return False
