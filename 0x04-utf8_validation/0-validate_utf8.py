c964e1a201017bbd915f118c29e92453d5409b45
zihco <chizobajames21@gmail.com>
2024-01-30 16:03:45 +0100

    utf-8 validation

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,7 @@ def validUTF8(data):
     """return a boolean to validate data utf8"""
     if type(data) is list:
         for element in data:
-            if type(element) is int and element < 256:
+            if type(element) is int and (element > 0 and element < 256):
                 continue
             else:
                 return False
