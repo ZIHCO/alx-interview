28f88cf487a7c3caa52feb0ba8a7aa2005002762
zihco <chizobajames21@gmail.com>
2024-05-09 21:52:32 +0100

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,12 +9,28 @@ def island_perimeter(grid):
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j]:
-                    if not grid[i - 1][j]:
-                        count += 1
-                    if not grid[i][j - 1]:
-                        count += 1
-                    if not grid[i][j + 1]:
-                        count += 1
-                    if not grid[i + 1][j]:
-                        count += 1
+                    try:
+                        top = grid[i - 1][j]
+                        if not top:
+                            count += 1
+                    except Exception:
+                        pass
+                    try:
+                        left = grid[i][j - 1]
+                        if not left:
+                            count += 1
+                    except Exception:
+                        pass
+                    try:
+                        right = grid[i][j + 1]
+                        if not right:
+                            count += 1
+                    except Exception:
+                        pass
+                    try:
+                        bottom = grid[i + 1][j]
+                        if not bottom:
+                            count += 1
+                    except Exception:
+                        pass
     return count
